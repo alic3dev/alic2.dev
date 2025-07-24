3703b002b730aebe0f722cfd3fae53c8e58b5d34
alic3 <alic3dev@gmail.com>
2025-07-23 22:39:53 -0700

    mesh_audio:add

--- alic2.js
+++ alic2.js
@@ -1,5 +1,28 @@
 const import_three = import('three');
 
+let vertices_alic2 = new Float32Array([
+  0, 0, 0,
+  1, 0, 0,
+  0, 1, 0,
+
+  0, 1, 0,
+  1, 1, 0,
+  1, 0, 0,
+]);
+
+const context_audio = new AudioContext();
+const buffer_audio = new AudioBuffer({numberOfChannels: 1, length: vertices_alic2.length * 6, sampleRate: 3000});
+const node_source_buffer = new AudioBufferSourceNode(context_audio);
+
+function start_audio_content() {
+  if (!node_source_buffer.buffer) {
+    node_source_buffer.buffer = buffer_audio;
+    node_source_buffer.loop = true;
+    node_source_buffer.connect(context_audio.destination);
+    node_source_buffer.start();
+  }
+}
+
 async function minone_initialize({three}) {
   const renderer = new three.WebGLRenderer();
 
@@ -65,6 +88,30 @@ async function minone_initialize({three}) {
 
   scene.add(light);
 
+  const geometry_alic2 = new three.BufferGeometry();
+
+  geometry_alic2.setAttribute(
+    'position',
+    new three.BufferAttribute(
+      vertices_alic2,
+      3
+    )
+  );
+
+  const material_alic2 = new three.MeshBasicMaterial({
+    color: 0xffffff,
+    side: three.DoubleSide
+  });
+
+  const mesh_alic2 = new three.Mesh(
+    geometry_alic2,
+    material_alic2
+  );
+
+  mesh_alic2.position.set(0, 2, -10);
+
+  scene.add(mesh_alic2);
+
   renderer.setAnimationLoop((
     time,
   ) => {
@@ -72,7 +119,28 @@ async function minone_initialize({three}) {
       scene,
       camera,
     )
-  })
+
+    for (
+      let index_vertex = 0;
+      index_vertex < vertices_alic2.length;
+      ++index_vertex
+    ) {
+      vertices_alic2[index_vertex] = vertices_alic2[index_vertex] > 0 ? (Math.random() + 0.01) : 0
+      buffer_audio.getChannelData(0)[index_vertex * 6] = vertices_alic2[index_vertex]
+    }
+
+    geometry_alic2.setAttribute(
+      'position',
+      new three.BufferAttribute(
+        vertices_alic2,
+        3
+      )
+    );
+  });
+
+  renderer.domElement.addEventListener('click', function() {
+    start_audio_content();
+  });
 
   document.body.append(renderer.domElement);
 }
